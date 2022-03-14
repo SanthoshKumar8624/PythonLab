1963da749e1d8ab6a3211aae5e5296aebf865ff7
Abubakersiddique761 <Abubakersiddique761@gmail.com>
2022-03-14 16:19:34 +0530

    Updated

---
+++ QS-8.py
@@ -0,0 +1,22 @@
+"""
+Question: Write a Program to find the largest of 3 Numbers
+"""
+
+n1 = int(input("Enter 1: "))
+n2 = int(input("Enter 2: "))
+n3 = int(input("Enter 3: "))
+
+
+print("\nThe Largest Number is: ", max(n1, n2, n3))
+
+
+"""
+OUTPUT:
+-----
+
+Enter 1: 23
+Enter 2: 12
+Enter 3: 56
+
+The Largest Number is:  56
+"""

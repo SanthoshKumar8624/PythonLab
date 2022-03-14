c29afcd1ccd87a95de0321c9c4ca3e99f939daaf
Abubakersiddique761 <Abubakersiddique761@gmail.com>
2022-03-14 16:24:51 +0530

    Updated

---
+++ QS-9.py
@@ -0,0 +1,14 @@
+"""
+Question: Write a program to convert Celsius to Fahrenheit
+"""
+
+celsius = int(input("Enter Celsius: "))
+print(f"Fahrenheit for {celsius} is: {(celsius * 1.8) + 32}")
+
+"""
+OUTPUT:
+-----
+
+Enter Celsius: 30
+Fahrenheit for 30 is: 86.0
+"""
